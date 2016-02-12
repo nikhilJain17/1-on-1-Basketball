a130ca8cc0c08e6858fcb8c51865bba366bad1ea
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-02-12 18:31:25 -0500

    the box moves with arrow keys wow

--- main.js
+++ main.js
@@ -18,19 +18,49 @@ function component(width, height, color, x, y) {
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
     
+     this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    
 }
+
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),   
-    
+    canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 1000;
+        this.canvas.width = 666;
         this.canvas.height = 666;
         this.context = this.canvas.getContext("2d");
-        
-        this.canvas.textContent = "sankwroot";
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]); // wtf does this line do
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        // to move the things with keys
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
+    
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-
 }
+
+
+function updateGameArea() {
+    myGameArea.clear();
+//    player1.x;
+    
+    if (myGameArea.key && myGameArea.key == 37) {player1.x += -5; }
+    if (myGameArea.key && myGameArea.key == 39) {player1.x += 5; }
+    if (myGameArea.key && myGameArea.key == 38) {player1.y += -5; }
+    if (myGameArea.key && myGameArea.key == 40) {player1.y += 5; }
+    
+    
+    player1.update();
+}
+ 
 
